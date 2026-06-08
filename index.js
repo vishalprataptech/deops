4beae1ddef7e0028e71f4103e6ac5fdd4264936d
Vishal Pratap Singh <vpsingh3445@gmail.com>
2026-06-08 09:27:30 +0530

    ui desine

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
+import path from 'node:path';
 
 const app = express();
 const server = createServer(app);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve( 'index.html')));
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.resolve('index.html'));
 });
 
 app.listen(3000, () => {
